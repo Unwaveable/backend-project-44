b5dd816353dce7c3a5267b3e20946a0168479c32
waveable <alex.waveabl3@gmail.com>
2025-01-22 00:26:54 +0300

    released brain-even 0.1

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
+import readlineSync from "readline-sync";
+import { getAndPrintUserName } from "./src/cli.js";
 
-console.log("Hello!");
+const correctAnswersToWin = 3;
+
+const userName = getAndPrintUserName();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+for (let countUserWins = 0; countUserWins < correctAnswersToWin; ) {
+  let num = Math.floor(Math.random() * 101);
+  console.log(`Question: ${num}`);
+  let userAnswer = readlineSync.question("Your answer: ");
+  let correctAnswer = num % 2 === 0 ? "yes" : "no";
+
+  if (userAnswer === correctAnswer) {
+    console.log("Correct!");
+    countUserWins += 1;
+  } else {
+    console.log(
+      `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}\nLet's' try again, ${userName}!`
+    );
+    break;
+  }
+  if (countUserWins === 3) {
+    console.log(`Congratulations, ${userName}`);
+  }
+}
