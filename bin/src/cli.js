5ec84a7bb24678b3087e5288216ac885d6a2cef8
waveable <alex.waveabl3@gmail.com>
2025-01-25 17:08:15 +0300

    fix greetings

--- bin/src/cli.js
+++ bin/src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from "readline-sync";
 
 export function getAndPrintUserName() {
-  const userName = readlineSync.question("May I have your name? ");
+  const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
   console.log(`Hello, ${userName}!`);
   return userName;
 }
